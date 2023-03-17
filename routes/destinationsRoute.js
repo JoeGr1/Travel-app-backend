a8e445149122ce3fcd422a0c6f1cb362d7022df7
Joe Gregson <joegregson@icloud.com>
2023-03-17 11:47:59 +0000

    finished back end

--- routes/destinationsRoute.js
+++ routes/destinationsRoute.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const fs = require("fs");
 
+const { v4: uuid } = require("uuid");
+
 const router = express.Router();
 
 router.get("/", (_req, res) => {
@@ -15,6 +17,51 @@ router.get("/", (_req, res) => {
 
     const destinations = JSON.parse(data);
     res.json(destinations);
+  });
+});
+
+router.post("/:id/comments", (req, res) => {
+  const paramId = req.params.id;
+  const { name, comment } = req.body;
+
+  const newComment = {
+    name: name,
+    comment: comment,
+    timestamp: Date.now(),
+    id: uuid(),
+  };
+
+  fs.readFile("./data/destinations.json", null, (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: true,
+        message: "Server error: could not retrieve data from JSON file",
+      });
+    }
+
+    const destinations = JSON.parse(data);
+    const destination = destinations.find(
+      (destination) => destination.id === paramId
+    );
+
+    destination.comments.push(newComment);
+
+    console.log(destination.comments);
+
+    fs.writeFile(
+      "./data/destinations.json",
+      JSON.stringify(destinations),
+      (err) => {
+        if (err) {
+          return res.status(500).json({
+            error: true,
+            message: "Server error: could not retrieve data from JSON file",
+          });
+        }
+
+        res.status(201).json(destination);
+      }
+    );
   });
 });
 
